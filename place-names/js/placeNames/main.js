0a0776b5c785c4a226449e49ba0036a7076e766c
Tommy Spurlock <tspurloc@gmail.com>
2022-01-21 09:57:53 -0500

    part one finished

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,19 @@
 // Put your code here
+
+const names = [ "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+
+
+    for( let name of names){
+        console.log(name)
+    }
+
+
+    theNames = names.filter(n => n.startsWith("The"))
+
+    console.log("-------------------------------------")
+
+    for( let name of theNames){
+        console.log(name)
+    }
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,69 @@
 // Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function Roll()
+{
+
+    let die = getRandomInt(1,6);
+    return die;
+}
+
+function ToString(number)
+{
+    let dieString = "Unknown";
+
+    // This "switch" statement behaves like a series of "if/else if" statements.
+    //  We compare the numeric Value of the current object accross different "cases".
+    //  When we find a matching case we return a string that represents an image of a die
+    // Did you kow that JavaScript also has "switch" statements?
+    switch (number)
+    {
+        /* "case 1:" is like the following "if" statement
+        if (this.Value == 1) {
+            dieString = "\u2680";
+        }
+        */
+        case 1:
+            dieString = "\u2680"; // These \uXXXX values are fancy unicode characters
+            break;
+        case 2:
+            dieString = "\u2681";
+            break;
+        case 3:
+            dieString = "\u2682";
+            break;
+        case 4:
+            dieString = "\u2683";
+            break;
+        case 5:
+            dieString = "\u2684";
+            break;
+        case 6:
+            dieString = "\u2685";
+            break;
+    }
+
+    return dieString;
+}
+
+for (let i = 0; i < 10; i++)
+{
+    let die1 = Roll();
+    let die2 = Roll();
+
+    let message = `${ToString(die1)} + ${ToString(die2)} == ${die1 + die2}`;
+    if (die1 == die2)
+    {
+        message += " DOUBLES!";
+    }
+
+    console.log(message);
+}
